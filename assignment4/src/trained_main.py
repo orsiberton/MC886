2fcd8b038542867b87191ede8259c870cd49b050
Bruno Berton <bberton@ciandt.com>
2018-05-27 12:53:08 -0300

    add report

--- assignment4/src/trained_main.py
+++ assignment4/src/trained_main.py
@@ -1,44 +1,57 @@
-from keras.applications.inception_v3 import InceptionV3
-from keras.preprocessing.image import ImageDataGenerator
-from keras import optimizers
-import matplotlib.pyplot as plt
+import os
+
+import numpy as np
 from keras.models import load_model
-from keras import models
-from keras import layers
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import classification_report
 
 img_width, img_height = 256, 256
 
 # Returns a compiled model identical to the previous one
-model = load_model('small_last4.h5')
+model = load_model('inception_v3_flat_relu_soft.h5')
 
 train_dir = "../data/train/resized"
 val_dir = "../data/val/resized"
 test_dir = "../data/test/resized"
 
-validation_datagen = ImageDataGenerator(rescale=1./255)
+validation_datagen = ImageDataGenerator(rescale=1. / 255)
 val_batchsize = 10
 
 validation_generator = validation_datagen.flow_from_directory(
-        val_dir,
-        target_size=(img_width, img_height),
-        batch_size=val_batchsize,
-        class_mode='categorical',
-        shuffle=False)
+    val_dir,
+    target_size=(img_width, img_height),
+    batch_size=val_batchsize,
+    class_mode='categorical',
+    shuffle=False)
 
 results = model.evaluate_generator(
     validation_generator,
-    steps=validation_generator.samples/validation_generator.batch_size,
+    steps=validation_generator.samples / validation_generator.batch_size,
     workers=4,
     use_multiprocessing=True)
 
 predictions = model.predict_generator(
     validation_generator,
-    steps=validation_generator.samples/validation_generator.batch_size,
+    steps=validation_generator.samples / validation_generator.batch_size,
     workers=4,
     use_multiprocessing=True)
 
-print model.metrics_names
-print results
+print(model.metrics_names)
+print(results)
 
-print "Predictions"
-print predictions
+print("Predictions")
+print(predictions)
+
+y_pred = [np.argmax(pred) for pred in predictions]
+
+class_names = []
+for i in range(83):
+    class_names.append("Class {0:02d}".format(i))
+
+y_true = []
+
+for file in sorted(os.listdir("../data/val")):
+    if file[-4:] == ".jpg":
+        y_true.append(int(file[0:2]))
+
+print(classification_report(y_true, y_pred, target_names=class_names))
